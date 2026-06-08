362ce71b5484c65280a9201b71488c1874041192
Ivan Roman <DigitalHerencia@Outlook.com>
2026-06-07 21:44:01 -0700

    Normalize requirement notice spacing ownership

--- components/shared/requirement-notice-split.tsx
+++ components/shared/requirement-notice-split.tsx
@@ -22,7 +22,7 @@ export function RequirementNoticeSplit({
   returnPath,
 }: RequirementNoticeSplitProps) {
   return (
-    <section className="px-4 py-8 md:px-8 lg:px-16">
+    <section>
       <div className="overflow-hidden border-3 border-neutral-400 bg-black shadow-[8px_8px_0px_oklch(54.6%_0.245_262.881)]">
         <div className="grid md:grid-cols-[minmax(0,1fr)_minmax(14rem,0.42fr)]">
           <div className="flex flex-col justify-center p-5 md:p-6">
